5ed614aadd7a3658c630d5edd52a7f5860b42650
Rodrigo Guerreiro <rodrigopg8@gmail.com>
2025-02-23 17:15:31 +0100

    Add Input UI component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,14 @@
 import './App.css'
+import { Input } from './components/ui/input'
 
 function App() {
 
   return (
-    <div className='bg-gray-200 h-screen flex items-center justify-center rounded'>
-      <img src='/imago.svg' className='w-40' alt='Imago Logo' />
+    <div className='bg-gray-200 h-screen flex flex-col items-center rounded p-4'>
+      <img src='/imago.svg' className='w-40 mb-4' alt='Imago Logo' />
+      <div>
+        <Input placeholder='Search' />
+      </div>
     </div>
   )
 }
