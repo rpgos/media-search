9e6ef0e2c0b7798d6e374481c71dcd7894e35a76
Rodrigo Guerreiro <rodrigopg8@gmail.com>
2025-02-23 21:16:43 +0100

    Show image results

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,17 +2,31 @@ import { useState } from 'react'
 import './App.css'
 import { SearchBar } from './components/SearchBar'
 import { useGetMedia } from './hooks/useGetMedia'
+import { Media } from './api/@types'
 
 function App() {
   const [searchTerm, setSearchTerm] = useState('')
 
   const { data } = useGetMedia({ query: searchTerm })
 
+  let images: Media[] = []
+
+  if(data?.pages) {
+    images = data?.pages.reduce(page => page)
+  }
+
   return (
     <div className='h-screen flex flex-col items-center rounded p-4'>
       <img src='/imago.svg' className='w-40 mb-4' alt='Imago Logo' />
       <p className="font-bold mb-4">Search through our collection of high-quality images</p>
       <SearchBar onSubmit={(query) => setSearchTerm(query)} />
+      <div className='flex flex-wrap gap-4 mt-4 justify-center'>
+        {
+          images.map((image) => (
+            <img key={image.id} src={image.image_url} alt={image.description} className='rounded' />
+          ))
+        }
+      </div>
     </div>
   )
 }
---
+++ frontend/src/api/@types.d.ts
@@ -0,0 +1,9 @@
+export type Media = {
+  id: string
+  description: string
+  date: string
+  photographer: string
+  image_url: string
+  width: number
+  height: number
+}
