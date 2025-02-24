2d199cce9aa88ee56b4232e284b59eeb1f718e9f
Rodrigo Guerreiro <rodrigopg8@gmail.com>
2025-02-24 11:21:55 +0100

    Refactor controller

--- app/controllers/api/v1/media_controller.rb
+++ app/controllers/api/v1/media_controller.rb
@@ -3,16 +3,12 @@
 module Api
   module V1
     class MediaController < ApplicationController
+      before_action :ensure_valid_query_param
+
       def index
-        query = params[:query]
         page = params[:page] || 1
 
-        if query.blank?
-          render json: { error: 'Query parameter is required' }, status: :bad_request
-          return
-        end
-
-        results = ElasticsearchService.search(query, filters: filter_params, page: page)
+        results = ElasticsearchService.search(es_query_param, filters: filter_params, page: page)
         render json: results, status: :ok
       rescue StandardError => e
         render json: { error: 'Something went wrong', details: e.message }, status: :internal_server_error
@@ -20,8 +16,18 @@ module Api
 
       private
 
+      def ensure_valid_query_param
+        return unless es_query_param.blank?
+
+        render json: { error: 'Query parameter is required' }, status: :bad_request and return
+      end
+
       def filter_params
         params.permit(:photographer, :db, :start_date, :end_date, :sort_direction)
+      end
+
+      def es_query_param
+        params[:query]
       end
     end
   end
